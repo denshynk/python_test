00fcb9aa30935c8be09e0a904f0a847213e3d6a3
denshynk <denshynk@gmail.com>
2022-05-26 23:58:16 +0300

    commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print(1)
+import os
+
+# Указываем папку в которой производить поиск
+path='C:/'
+
+for rootdir, dirs, files in os.walk(path):
+    for file in files:       
+        if((file.split('.')[-1])=='mp3'):
+            print (os.path.join(rootdir, file))
